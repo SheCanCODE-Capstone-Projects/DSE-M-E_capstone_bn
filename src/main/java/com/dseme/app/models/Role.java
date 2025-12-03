b0cd38d633bf2f9dca7f9f21d10a09e1c0bfb73f
Nicole0509 <niashavril0509@gmail.com>
2025-12-03 22:50:54 +0200

    Feat:sign up

---
+++ src/main/java/com/dseme/app/models/Role.java
@@ -0,0 +1,8 @@
+package com.dseme.app.models;
+
+public enum Role {
+    PARTNER,            //MasterCard
+    ME_OFFICER,         //Organisations
+    FACILITATOR,        //Participant Instructor
+    UNASSIGNED          //Intermediate Role
+}
