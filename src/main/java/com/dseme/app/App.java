ae6be89319a171b5d4ef0dd7d820c5abae134c94
Nicole0509 <niashavril0509@gmail.com>
2025-12-18 22:11:24 +0200

    feature:deploy Configuration for Railway Deployment

--- src/main/java/com/dseme/app/App.java
+++ src/main/java/com/dseme/app/App.java
@@ -1,13 +1,9 @@
 package com.dseme.app;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-})
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
