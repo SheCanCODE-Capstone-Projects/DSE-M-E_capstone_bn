b05f5ccbbbfc1afc7354f76ccb33ba1a2901d485
Eddy Dashner <education@shecancodeschool.org>
2026-02-05 23:09:53 +0200

    Update Swagger configuration for deployment

--- src/main/java/com/dseme/app/configurations/SwaggerConfig.java
+++ src/main/java/com/dseme/app/configurations/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -18,9 +21,13 @@ public class SwaggerConfig {
                         .title("DSE M&E Platform API")
                         .version("1.0")
                         .description("Digital Skills for Employability Monitoring & Evaluation Platform API"))
+                .servers(List.of(
+                        new Server().url("https://dse-m-ecapstonebn-production.up.railway.app").description("Production Server"),
+                        new Server().url("http://localhost:8080").description("Local Development Server")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components()
-                        .addSecuritySchemes("Bearer Authentication", 
+                        .addSecuritySchemes("Bearer Authentication",
                                 new SecurityScheme()
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
